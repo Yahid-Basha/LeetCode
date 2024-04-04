f638d0cf949de4df0fd52dcb062752aac16acce7
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2024-04-04 15:29:35 +0530

    Time: 1 ms (98.65%) | Memory: 41.2 MB (87.52%) - LeetSync

--- SQL/20-valid-parentheses/valid-parentheses.java
+++ SQL/20-valid-parentheses/valid-parentheses.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i =0; i<s.length();i++){
-            char c = s.charAt(i);
+        for(char c: s.toCharArray()){
             if(c == '(' || c == '{' || c == '[' ){
                 st.push(c);
             } else if(st.isEmpty()){
