6e761fdd8628258506d856d221df84a638907cb3
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2024-06-08 23:31:51 +0530

    Time: 2 ms (97.36%) | Memory: 43.7 MB (60.83%) - LeetSync

--- SQL/242-valid-anagram/valid-anagram.java
+++ SQL/242-valid-anagram/valid-anagram.java
@@ -2,9 +2,17 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
         char sa[] = s.toCharArray();
-        Arrays.sort(sa);
+        int count[] = new int[26];
+        for(char c: sa){
+            count[c-'a']++;
+        }
         char ta[] = t.toCharArray();
-        Arrays.sort(ta);
-        return Arrays.equals(sa,ta);
+        for (char d: ta){
+            if(count[d-'a']==0){
+                return false;
+            }
+            count[d-'a']--;
+        }
+        return true;
     }
 }
