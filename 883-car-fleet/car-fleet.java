fc5be016ff31f91bf084cd16fe5581cd72a0b3b1
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2025-01-06 18:27:15 +0530

    Time: 98 ms (23.50%) | Memory: 55.8 MB (87.20%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,19 +1,22 @@
-public class Solution {
+class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int[][] pair = new int[position.length][2];
-        for (int i = 0; i < position.length; i++) {
+        int n = speed.length;
+        int [][] pair = new int[n][2];
+        if( n == 1) return n;
+        for(int i = 0; i < n; i++){
             pair[i][0] = position[i];
             pair[i][1] = speed[i];
         }
-        Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
+        Arrays.sort(pair, (a,b)->Integer.compare(b[0], a[0]));
+
         Stack<Double> stack = new Stack<>();
-        for (int[] p : pair) {
-            stack.push((double) (target - p[0]) / p[1]);
-            if (stack.size() >= 2 && 
-                stack.peek() <= stack.get(stack.size() - 2)) 
-            {
+        for(int[] p: pair){
+            double time = (double)(target-p[0])/p[1];
+            stack.push(time);
+            if(stack.size() >= 2 && stack.peek() <= stack.get(stack.size()-2)){
                 stack.pop();
             }
+
         }
         return stack.size();
     }
