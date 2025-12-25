551ef4378911fd4cab76282c02e73fbc67f2a0ba
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2025-12-26 01:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (46.96%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev1 = nums[0];
+        if(nums.length < 2) return nums[0];
+        int prev2 = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.length; i++){
+            int curr = Math.max(prev2, prev1+nums[i]);
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+}
