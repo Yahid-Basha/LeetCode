4124a880c286e9d4e3760e94334bc4d350b4ed1c
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-01-01 12:33:38 +0530

    Time: 5 ms (97.89%) | Memory: 44.1 MB (24.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -5,15 +5,15 @@ class Solution {
         int[] posOfS = new int[256];
         int[] posOfT = new int[256];
 
-        Arrays.fill(posOfS, -1);
-        Arrays.fill(posOfT, -1);
+        // Arrays.fill(posOfS, -1);
+        // Arrays.fill(posOfT, -1);
 
         for(int i = 0; i < n; i++){
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
-            if(posOfS[sChar] != posOfT[tChar]) return false;
-            posOfS[sChar] = i;
-            posOfT[tChar] = i;
+            if(posOfS[sChar]-1 != posOfT[tChar]-1) return false;
+            posOfS[sChar] = i+1;
+            posOfT[tChar] = i+1;
         }
         return true;
     }
