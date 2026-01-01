831b6c5886f2e1f5790f88fe2db9f0c0c3067c89
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-01-01 13:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (94.76%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n+1];
+
+        for(int i: citations){
+            if(i >= n) buckets[n]++;
+            else{
+                buckets[i]++;
+            }
+        }
+        int count = 0;
+        for(int i = n; i > -1; i--){
+            count += buckets[i];
+            if(count >= i) return i;
+        }
+        return 0;
+    }
+}
