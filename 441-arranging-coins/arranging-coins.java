fb2ef570deca90fbef27cc8e214ae8a1aec51f93
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-01-17 13:28:58 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (29.61%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,11 +1,22 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int row = 1;
-        while(n > 0){
-            n = n-row;
-            if(n > 0) row++;
-            // row++;
+        long start = 0;
+        long end = n;
+        
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+            long coinsNeeded = mid * (mid + 1) / 2;
+            
+            if(coinsNeeded == n) return (int)mid;
+            if(coinsNeeded < n){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
-        return n < 0 ? row-1 : row;
+        
+        // Since we want the completed rows, 'end' will point to the largest k 
+        // where k*(k+1)/2 <= n after the loop breaks.
+        return (int) end; 
     }
 }
