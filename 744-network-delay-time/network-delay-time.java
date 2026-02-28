284f9ccc93644c6236d676fd5bdc804e05f285ce
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-02-28 17:41:27 +0530

    Time: 10 ms (85.48%) | Memory: 49 MB (73.84%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -3,18 +3,35 @@ class Solution {
         int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
-        for(int i = 0; i < n; i++){
-            for(int[] edge: times){
-                int u = edge[0], v = edge[1], w = edge[2];
-                if(dist[u] != Integer.MAX_VALUE && dist[v] > dist[u]+w){
-                    dist[v] = dist[u]+w;
-                }
+
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : times){
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj.get(u).add(new int[]{v, w});
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        pq.add(new int[]{k, 0});
+
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int u = curr[0];
+            if(curr[1] > dist[u]) continue;
+            for(int[] edge: adj.get(u)){
+                int v = edge[0], w = edge[1];
+                if(dist[v] <= dist[u]+w) continue;
+                dist[v] = dist[u]+w;
+                pq.offer(new int[] {v, dist[v]});
             }
         }
         int time = 0;
-        dist[0]=0;
-        for(int t: dist){
-            time = Math.max(t, time);
+        
+        for(int i =1; i < n+1; i++){
+            time = Math.max(dist[i], time);
         }
         return time == Integer.MAX_VALUE ? -1 : time;
     }
