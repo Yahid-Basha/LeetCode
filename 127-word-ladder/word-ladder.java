1b6596058dd08846647ea7bc1ee241718b51ebf5
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-01 13:45:49 +0530

    Time: 1349 ms (5.01%) | Memory: 85.6 MB (5.04%) - LeetSync

---
+++ 127-word-ladder/word-ladder.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> visited = new HashSet<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: wordList){
+            for(int i = 0; i < s.length(); i++){
+                String key = s.substring(0,i)+"*"+s.substring(i+1);
+                if(!map.containsKey(key))
+                    map.put(key, new ArrayList<>());
+                map.get(key).add(s);
+            }
+        }
+        
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int level = 1;
+        visited.add(beginWord);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int k = 0; k < size; k++){
+                String s = queue.poll();
+                for(int i = 0; i < s.length(); i++){
+                    String key = s.substring(0,i)+"*"+s.substring(i+1);
+                    if(!map.containsKey(key)) continue;
+                    for(String node: map.get(key)){
+                        if(node.equals(endWord)) return level+1;
+                        if(!visited.contains(node)){
+                            queue.offer(node);
+                        }
+                    }
+                }
+                visited.add(s);
+            }
+            level++;
+        }
+        return 0;
+    }
+}
