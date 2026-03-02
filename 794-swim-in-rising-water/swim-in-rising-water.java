5d8d34718b6e588dd3f26a8d89210593cda7d445
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-03 00:03:54 +0530

    Time: 78 ms (5.05%) | Memory: 47.2 MB (6.99%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -8,12 +8,14 @@ class Solution {
         while(!minHeap.isEmpty()){
             int[] curr = minHeap.poll();
             visited[curr[0]][curr[1]] = true;
-            if(curr[0] == grid.length -1 && curr[1] == grid[0].length-1 ) return curr[2];
+            System.out.print(curr[2]+" ");
+            time = Math.max(time, curr[2]);
+            if(curr[0] == grid.length -1 && curr[1] == grid[0].length-1 ) return time;
             for(int dir[]: dirs){
                 int r = curr[0]+dir[0];
                 int c = curr[1]+dir[1];
                 if(c >= 0 && r >= 0 && c < grid[0].length && r < grid.length && !visited[r][c]){
-                    minHeap.offer(new int[]{r, c,  Math.max(curr[2], grid[r][c])});
+                    minHeap.offer(new int[]{r, c, grid[r][c]});
                 }
             }
         }
