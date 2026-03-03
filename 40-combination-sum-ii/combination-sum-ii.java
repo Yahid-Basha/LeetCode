43a7410c70a59e3b6145cf18b57e9f695b33ef87
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-03 22:43:05 +0530

    Time: 6 ms (73.67%) | Memory: 45 MB (87.84%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(candidates, 0, target, 0, new ArrayList<>(), res);
+        return res;
+    }
+    void dfs(int[] candidates,int start, int target, int currSum, List<Integer> ans, List<List<Integer>> res){
+        if(target == currSum){
+            res.add(new ArrayList<>(ans));
+            return;
+        }
+        for(int i = start; i < candidates.length; i++){
+            if( i > start && candidates[i] == candidates[i-1]) continue;
+            if(currSum + candidates[i] > target) break;
+            ans.add(candidates[i]);
+            dfs(candidates, i+1, target, currSum+candidates[i],ans, res);
+            ans.remove(ans.size()-1);
+        }
+
+    }
+}
