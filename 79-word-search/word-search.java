384e7c7a65e154933606980d638a5f65f419bb43
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-03 22:27:18 +0530

    Time: 135 ms (59.65%) | Memory: 43.2 MB (29.93%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,40 +1,33 @@
 class Solution {
-
     public boolean exist(char[][] board, String word) {
-        String ans = new String("");
-        int m = board.length;
-        int n = board[0].length;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (backtrack(board,i, j, 0, word, m, n)) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(dfs(board, i, j, 0, word)){
                         return true;
                     }
                 }
             }
         }
-
         return false;
     }
+    boolean dfs(char[][] board, int r, int c, int i, String word){
+        if(i == word.length()) return true;
+        if(r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] == '#'|| i >= word.length() ) return false;
+        if(board[r][c] != word.charAt(i)) return false;
 
-    public boolean backtrack(char[][] board, int i, int j, int idx,String word, int m, int n){
-        if(i < 0|| i >= m || j < 0 || j >= n||board[i][j]!=word.charAt(idx)) return false;
-        if(idx == word.length()-1) return true;
-
-        board[i][j] = '#';
-
-        if(
-            backtrack(board,i-1,j,idx+1,word, m, n) || 
-            backtrack(board,i+1,j,idx+1,word, m, n) || 
-            backtrack(board,i,j-1,idx+1,word, m, n) || 
-            backtrack(board,i,j+1,idx+1,word, m, n) 
-        ){
-            return true;
-        }
-
-        board[i][j] = word.charAt(idx);
+        char temp = board[r][c];
+        board[r][c] = '#';
+            if(
+                dfs(board, r+1, c, i+1, word) ||
+                dfs(board, r-1, c, i+1, word) ||
+                dfs(board, r, c+1, i+1, word) ||
+                dfs(board, r, c-1, i+1, word) 
+            ){
+                board[r][c] = temp;
+                return true;
+            }
+            board[r][c] = temp;
         return false;
-
     }
 }
