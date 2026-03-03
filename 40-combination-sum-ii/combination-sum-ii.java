93154bfefc2b68303e390741f530530fdf9386c4
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-03 22:44:53 +0530

    Time: 5 ms (99.23%) | Memory: 45.3 MB (68.33%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -6,14 +6,13 @@ class Solution {
         return res;
     }
     void dfs(int[] candidates,int start, int target, int currSum, List<Integer> ans, List<List<Integer>> res){
-        if(currSum > target) return;
         if(target == currSum){
             res.add(new ArrayList<>(ans));
             return;
         }
         for(int i = start; i < candidates.length; i++){
             if( i > start && candidates[i] == candidates[i-1]) continue;
-            // if(currSum + candidates[i] > target) break;
+            if(currSum + candidates[i] > target) break;
             ans.add(candidates[i]);
             dfs(candidates, i+1, target, currSum+candidates[i],ans, res);
             ans.remove(ans.size()-1);
