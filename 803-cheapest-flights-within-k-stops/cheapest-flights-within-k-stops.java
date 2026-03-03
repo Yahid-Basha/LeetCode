1009ce0488c99a50006dd9d38511d34934970cc1
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-03 13:14:14 +0530

    Time: 5 ms (98.55%) | Memory: 46.9 MB (15.76%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,17 +1,18 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
-        for(int i = 0; i < k+1; i++){
-            int[] temp = dist.clone();
-            for(int[] edge: flights){
-                int u = edge[0], v = edge[1], w = edge[2];
-                if(temp[u] != Integer.MAX_VALUE && temp[u]+w < dist[v])
-                    dist[v] = temp[u]+w;
+        int prices[] = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        prices[src] = 0;
+
+        for(int i = 0; i <= k; i++){
+            int[] dist = prices.clone();
+            for(int[] flight: flights){
+                int u = flight[0], v= flight[1], w = flight[2];
+                if(dist[u] != Integer.MAX_VALUE && dist[u]+w < prices[v]){
+                    prices[v] = dist[u]+w;
+                }
             }
         }
-        // return dist[dst];
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
     }
 }
