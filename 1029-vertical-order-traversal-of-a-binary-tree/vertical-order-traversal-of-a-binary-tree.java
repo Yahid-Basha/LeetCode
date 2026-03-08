7f39e943de72d163c6d57201e705580133c0adbf
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-08 15:42:10 +0530

    Time: 10 ms (5.35%) | Memory: 50.2 MB (7.68%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -25,6 +25,7 @@ class Solution {
             this.node = node;
             this.row = row;
             this.col = col;
+            this.val = node.val;
         }
 
         int getCol(){
@@ -36,38 +37,43 @@ class Solution {
         }
 
         int getVal(){
-            return node.val;
+            return val;
         }
     }
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         Map<Integer, List<Integer>> map = new HashMap<>();
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(root, 0, 0));
         List<Node> list = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
         List<List<Integer>> ans = new ArrayList<>();
+
+        queue.offer(new Node(root, 0, 0));
         while(!queue.isEmpty()){
             int size = queue.size();
             for(int i = 0; i < size; i++){
                 Node node = queue.poll();
-                TreeNode curr = node.node;
-                int col = node.col, row = node.row;
                 list.add(node);
-                if(curr.left!= null){
+                TreeNode curr = node.node;
+                int col = node.col;
+                int row = node.row;
+                if(curr.left != null){
                     queue.offer(new Node(curr.left, col-1, row+1));
                 }
-                if(curr.right!= null){
+                if(curr.right != null){
                     queue.offer(new Node(curr.right, col+1, row+1));
                 }
             }
         }
         list.sort(Comparator.comparing(Node::getCol).thenComparing(Node::getRow).thenComparing(Node::getVal));
         for(Node node: list){
-            map.computeIfAbsent(node.col, k -> new ArrayList<>()).add(node.node.val);
+            if(!map.containsKey(node.col)){
+                map.put(node.col, new ArrayList<>());
+            }
+            map.get(node.col).add(node.val);
         }
         List<Integer> sortedKeys = new ArrayList<>(map.keySet());
         Collections.sort(sortedKeys);
-        for(int key: sortedKeys){
+        for(int key : sortedKeys){
             ans.add(map.get(key));
         }
         return ans;
