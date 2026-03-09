d9e2f55de70fc4684e1ca74aa98209d2c7af24f8
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-09 23:44:39 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (26.68%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -16,6 +16,22 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if(root == null) return 0;
-        return countNodes(root.left) + countNodes(root.right)+1;
+        TreeNode curr = root;
+        int ldepth = 0;
+        while(curr != null){
+            ldepth++;
+            curr = curr.left;
+        }
+        int rdepth = 0;
+        curr = root;
+        while(curr != null){
+            rdepth++;
+            curr = curr.right;
+        }
+        if(ldepth == rdepth){
+            return (int)Math.pow(2, ldepth)-1;
+        }
+
+        return 1+countNodes(root.left)+countNodes(root.right);
     }
 }
