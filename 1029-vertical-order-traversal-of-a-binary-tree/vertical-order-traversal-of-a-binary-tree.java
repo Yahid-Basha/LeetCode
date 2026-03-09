77a271a6434ee64247a3593d07ba6c3d1e837a06
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-09 17:54:17 +0530

    Time: 15 ms (5.35%) | Memory: 50.4 MB (7.60%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -14,66 +14,60 @@
  * }
  */
 class Solution {
-
     class Node{
         TreeNode node;
         int row;
         int col;
         int val;
 
-        Node(TreeNode node, int col, int row){
+        Node(TreeNode node, int row, int col){
             this.node = node;
             this.row = row;
             this.col = col;
-            this.val = node.val;
-        }
-
-        int getCol(){
-            return col;
+            val = node.val;
         }
 
         int getRow(){
             return row;
         }
 
+        int getCol(){
+            return col;
+        }
+
         int getVal(){
             return val;
         }
     }
-
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
         List<Node> list = new ArrayList<>();
         Queue<Node> queue = new LinkedList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
         List<List<Integer>> ans = new ArrayList<>();
-
-        queue.offer(new Node(root, 0, 0));
+        if(root == null) return ans;
+        queue.add(new Node(root, 0, 0));
         while(!queue.isEmpty()){
             int size = queue.size();
             for(int i = 0; i < size; i++){
                 Node node = queue.poll();
                 list.add(node);
                 TreeNode curr = node.node;
-                int col = node.col;
-                int row = node.row;
-                if(curr.left != null){
-                    queue.offer(new Node(curr.left, col-1, row+1));
-                }
-                if(curr.right != null){
-                    queue.offer(new Node(curr.right, col+1, row+1));
-                }
+                int row = node.row, col = node.col;
+                if(curr.left != null) queue.offer(new Node(curr.left, row+1, col-1));
+                if(curr.right != null) queue.offer(new Node(curr.right, row+1, col+1));
             }
         }
         list.sort(Comparator.comparing(Node::getCol).thenComparing(Node::getRow).thenComparing(Node::getVal));
         for(Node node: list){
-            if(!map.containsKey(node.col)){
-                map.put(node.col, new ArrayList<>());
+            int key = node.col;
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
             }
-            map.get(node.col).add(node.val);
+            map.get(key).add(node.val);
         }
         List<Integer> sortedKeys = new ArrayList<>(map.keySet());
         Collections.sort(sortedKeys);
-        for(int key : sortedKeys){
+        for(int key: sortedKeys){
             ans.add(map.get(key));
         }
         return ans;
