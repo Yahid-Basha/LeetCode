5ba17704fe1f5c680748fff8667e66a6fd300574
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-03-10 22:11:41 +0530

    Time: 8 ms (91.00%) | Memory: 49 MB (82.07%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -5,13 +5,11 @@ class Solution {
         Arrays.sort(intervals, (a,b)->a[0]-b[0]);
         ans.add(intervals[0]);
         for(int i = 0; i < intervals.length; i++){
-            int[] prev = ans.remove(ans.size()-1);
+            int[] prev = ans.get(ans.size()-1);
             int[] curr = intervals[i];
-
-            if(prev != null && curr[0] <= prev[1]){
-                ans.add(new int[]{prev[0], Math.max(curr[1], prev[1])});
+            if(curr[0] <= prev[1]){
+                prev[1] = Math.max(curr[1], prev[1]);
             }else{
-                ans.add(prev);
                 ans.add(curr);
             }
         }
