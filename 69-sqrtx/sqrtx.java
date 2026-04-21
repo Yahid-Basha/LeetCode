ab18133e8abf508289bb4906b8c7755214759a79
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-04-21 11:17:58 +0530

    Time: 1 ms (97.80%) | Memory: 42.3 MB (90.47%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        int ans = 0;
+        int left = 1, right = x;
+        while(left <= right){
+            int mid = left + (right -left)/2;
+            if(mid <= x/mid){
+                ans = mid;
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+}
