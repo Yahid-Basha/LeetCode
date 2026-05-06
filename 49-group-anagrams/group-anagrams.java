878e9c1fa3929578fe1a03755435618338c93140
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2026-05-06 22:49:21 +0530

    Time: 18 ms (23.97%) | Memory: 50.8 MB (12.61%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        for(String s: strs){
-            int[] count = new int[26];
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
-            }
-            String k = Arrays.toString(count);
-            if(map.containsKey(k)){
-                map.get(k).add(s);
-            } else{
-                List<String> list = new ArrayList<>();
-                list.add(s);
-                map.put(k, list);
-            }
+
+        
+        for(String str: strs){
+            String key = createKey(str);
+            
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
         }
+
         return new ArrayList<>(map.values());
+    }
+    public String createKey(String str){
+        int arr[] = new int[26];
+        for(int i = 0; i < str.length(); i++){
+            arr[str.charAt(i)-'a']++;
+        }
+        return Arrays.toString(arr);
     }
 }
